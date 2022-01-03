1f8eef523a867b2157408cbd54c486f4fd7d9764
icebear <kangnan5264626@gmail.com>
2022-01-03 21:33:16 +0800

    myFuncType

--- src/go_code/chapter06/funcdemo05/main.go
+++ src/go_code/chapter06/funcdemo05/main.go
@@ -3,12 +3,14 @@
  * @version:
  * @Author: Nan Kang
  * @Date: 2022-01-03 20:08:31
- * @LastEditTime: 2022-01-03 21:01:28
+ * @LastEditTime: 2022-01-03 21:24:39
  * @LastEditors: Do not edit
  */
 package main
 
 import "fmt"
+
+type myFunType func(int, int) int
 
 //在Go中,函数也是一种数据类型
 //可以复制给一个变量,则该变量就是一个函数类型的变量了.通过该变量对函数调用
@@ -16,6 +18,9 @@ func getSum(n1 int, n2 int) int {
 	return n1 + n2
 }
 func myFun(funvar func(int, int) int, num1 int, num2 int) int {
+	return funvar(num1, num2)
+}
+func myFun2(funvar myFunType, num1 int, num2 int) int {
 	return funvar(num1, num2)
 }
 func main() {
@@ -34,4 +39,8 @@ func main() {
 	num2 = int(num1)
 	fmt.Println("num1=", num1, "num2=", num2)
 
+	//再加一个案例
+
+	res3 := myFun2(getSum, 500, 600)
+	fmt.Println("res3=", res3)
 }

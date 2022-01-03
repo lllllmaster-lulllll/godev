cf984e15a683492b2505d409782abe2bab036ce0
icebear <kangnan5264626@gmail.com>
2022-01-03 20:23:06 +0800

    第二次上传readme文件

---
+++ src/go_code/README.md
@@ -0,0 +1,6 @@
+## github使用练习
+
+[本项目地址](https://github.com/lllllmaster-lulllll/godev.git)
+
+### 第一次上传
+
---
+++ src/go_code/chapter06/funcdemo05/main.go
@@ -0,0 +1,12 @@
+/*
+ * @Description:
+ * @version:
+ * @Author: Nan Kang
+ * @Date: 2022-01-03 20:08:31
+ * @LastEditTime: 2022-01-03 20:08:32
+ * @LastEditors: Do not edit
+ */
+package main
+
+//在Go中,函数也是一种数据类型
+//可以复制给一个变量,则该变量就是一个函数类型的变量了.通过该变量对函数调用
